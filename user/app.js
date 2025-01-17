1bd6d8939698d1493b5e5eb39888775a9a18527d
MouadAgn <103049879+MouadAgn@users.noreply.github.com>
2025-01-17 10:03:27 +0100

    Update

--- user/app.js
+++ user/app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 const PORT = 4000;
 app.use(cors({ origin: "*" }));
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/storedb", {
+mongoose.connect("mongodb://localhost:27017/storedb", {
 });
 console.log("Connected to MongoDB");
 
